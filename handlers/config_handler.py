db9bc67f76659c2b93a658cd58d9b5002faf3dbe
ImTheFrack <ryanjblack@hotmail.com>
2025-10-19 03:52:30 -0700

    fix config handler

--- handlers/config_handler.py
+++ handlers/config_handler.py
@@ -6,11 +6,18 @@ Returned JSON shape:
   "platform_filter_and": true|false
 }
 """
-from .. import config
+try:
+  import config
+except Exception:
+  # Fallback in case config isn't importable for some reason
+  class _C:
+    PLATFORM_FILTER_AND = False
+  config = _C()
+
 
 def handle(req):
-    # Only support GET
-    if req.get('method', 'GET').upper() != 'GET':
-        return (405, {'error': 'Method not allowed'})
+  # Only support GET
+  if req.get('method', 'GET').upper() != 'GET':
+    return (405, {'error': 'Method not allowed'})
 
-    return {'platform_filter_and': bool(getattr(config, 'PLATFORM_FILTER_AND', False))}
+  return {'platform_filter_and': bool(getattr(config, 'PLATFORM_FILTER_AND', False))}
